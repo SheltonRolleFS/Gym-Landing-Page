bd8e4d379a7c79788270d3dd473d2d9cd9f89977
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-3.local>
2022-01-02 17:45:55 -0500

    Added the content and styling for the welcome section of the landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
+import pose from "./images/gym-pose.png";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <section id="welcome">
+        <div id="yoga-pose">
+          <div id="circle" />
+          <img src={pose} alt="pose" />
+        </div>
+        <div id="welcome-text">
+          <header>
+            <h1>Welcome to,</h1>
+            <h1>Ronnies Gym</h1>
+          </header>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl
+            purus in mollis nunc. Posuere sollicitudin aliquam ultrices
+            sagittis. Donec et odio pellentesque diam volutpat. Consectetur a
+            erat nam at lectus
+          </p>
+          <div id="action-buttons">
+            <button>Schedule a Visit</button>
+            <button>View Plans</button>
+          </div>
+        </div>
+      </section>
+      <div className="divider" />
+    </>
   );
 }
 
